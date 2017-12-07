3f1a68ed74e626cfc9d26af388f79fbb6502ef74
julia <julia@DESKTOP-H6939SR.fritz.box>
2017-12-07 13:56:23 +0100

    Nummern können automatisch generiert werden

---
+++ ModevonMorgenSP/src/Datenbankverwaltung/holeNächsteNummer.java
@@ -0,0 +1,297 @@
+package Datenbankverwaltung;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * 
+ * 
+ * @author julian
+ *
+ */
+public class holeNächsteNummer {
+	
+	public static int nächsteMaNr(){
+		int i = 0;
+		
+		Connection con = VerbindungDB.erstelleConnection();
+		
+		try {
+			
+			Statement stmt = con.createStatement();
+			String sqlbefehl = "select max(nutzernr) from Mitarbeiter";
+			
+			ResultSet rs = stmt.executeQuery(sqlbefehl);
+			while (rs.next()) {
+				i = rs.getInt("Nutzernr");
+				i = i++;
+			}
+			
+			stmt.close();
+			con.close();
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return i;
+	}
+	
+	public static int nächsteAdminNr() {
+		int i = 0;
+		
+		Connection con = VerbindungDB.erstelleConnection();
+		
+		try {
+			
+			Statement stmt = con.createStatement();
+			String sqlbefehl = "select max(nutzernr) from Administrator";
+			
+			ResultSet rs = stmt.executeQuery(sqlbefehl);
+			while (rs.next()) {
+				i = rs.getInt("Nutzernr");
+				i = i++;
+			}
+			
+			stmt.close();
+			con.close();
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return i;
+	}
+	
+	public static int nächsteBKundenNr() {
+		int i = 0;
+		
+		Connection con = VerbindungDB.erstelleConnection();
+		
+		try {
+			
+			Statement stmt = con.createStatement();
+			String sqlbefehl = "select max(nutzernr) from Bestandskunde";
+			
+			ResultSet rs = stmt.executeQuery(sqlbefehl);
+			while (rs.next()) {
+				i = rs.getInt("Nutzernr");
+				i = i++;
+			}
+			
+			stmt.close();
+			con.close();
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return i;
+	}
+	
+	public static int nächste
+
+	public static int nächsteGKundenNr() {
+		
+		int i = 0;
+		
+		Connection con = VerbindungDB.erstelleConnection();
+		
+		try {
+			
+			Statement stmt = con.createStatement();
+			String sqlbefehl = "select max(nutzernr) from Gastkunde";
+			
+			ResultSet rs = stmt.executeQuery(sqlbefehl);
+			while (rs.next()) {
+				i = rs.getInt("Nutzernr");
+				i = i++;
+			}
+			
+			stmt.close();
+			con.close();
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return i;
+}
+	
+	public static int nächsteSchuhNr() {
+		int i = 0;
+		
+		Connection con = VerbindungDB.erstelleConnection();
+		
+		try {
+			
+			Statement stmt = con.createStatement();
+			String sqlbefehl = "select max(artikelnr) from Schuhe";
+			
+			ResultSet rs = stmt.executeQuery(sqlbefehl);
+			while (rs.next()) {
+				i = rs.getInt("artikelnr");
+				i = i++;
+			}
+			
+			stmt.close();
+			con.close();
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return i;
+	}
+		
+	public static int nächsteKleidungNr() {
+	
+		int i = 0;
+		
+		Connection con = VerbindungDB.erstelleConnection();
+		
+		try {
+			
+			Statement stmt = con.createStatement();
+			String sqlbefehl = "select max(artikelnr) from Kleidung";
+			
+			ResultSet rs = stmt.executeQuery(sqlbefehl);
+			while (rs.next()) {
+				i = rs.getInt("artikelnr");
+				i = i++;
+			}
+			
+			stmt.close();
+			con.close();
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return i;
+	}
+
+	public static int nächsteAccessNr() {
+		int i = 0;
+		
+		Connection con = VerbindungDB.erstelleConnection();
+		
+		try {
+			
+			Statement stmt = con.createStatement();
+			String sqlbefehl = "select max(artikelnr) from Accessoires";
+			
+			ResultSet rs = stmt.executeQuery(sqlbefehl);
+			while (rs.next()) {
+				i = rs.getInt("artikelnr");
+				i = i++;
+			}
+			
+			stmt.close();
+			con.close();
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return i;
+	
+	}
+
+	public static int nächsteBestellPosNr() {
+		
+		int i = 0;
+		
+		Connection con = VerbindungDB.erstelleConnection();
+		
+		try {
+			
+			Statement stmt = con.createStatement();
+			String sqlbefehl = "select max(bestellposnr) from Bestellposition";
+			
+			ResultSet rs = stmt.executeQuery(sqlbefehl);
+			while (rs.next()) {
+				i = rs.getInt("bestellposnr");
+				i = i++;
+			}
+			
+			stmt.close();
+			con.close();
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return i;
+	}
+
+	public static int nächsteRechnungsNr() {
+		
+		int i = 0;
+		
+		Connection con = VerbindungDB.erstelleConnection();
+		
+		try {
+			
+			Statement stmt = con.createStatement();
+			String sqlbefehl = "select max(rechnungsnr) from RechnungBestellung";
+			
+			ResultSet rs = stmt.executeQuery(sqlbefehl);
+			while (rs.next()) {
+				i = rs.getInt("Rechnungsnr");
+				i = i++;
+			}
+			
+			stmt.close();
+			con.close();
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return i;
+	}
+		
+	public static int nächsteBestellNr() {
+		int i = 0;
+		
+		Connection con = VerbindungDB.erstelleConnection();
+		
+		try {
+			
+			Statement stmt = con.createStatement();
+			String sqlbefehl = "select max(bestellnr) from RechnungBestellung";
+			
+			ResultSet rs = stmt.executeQuery(sqlbefehl);
+			while (rs.next()) {
+				i = rs.getInt("Bestellnr");
+				i = i++;
+			}
+			
+			stmt.close();
+			con.close();
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return i;
+	}
+	
+}
+
+	
+
+
