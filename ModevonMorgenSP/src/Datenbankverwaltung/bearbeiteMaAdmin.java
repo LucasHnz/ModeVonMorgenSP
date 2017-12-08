b9504c9518c1978b28b721bf9fce8cc1627624f9
julianhermann <julianhermann@julians-air.dhcp.fh-bielefeld.de>
2017-12-08 16:18:35 +0100

    Editiere Nutzer Steuerungsklassen

--- ModevonMorgenSP/src/Datenbankverwaltung/bearbeiteMaAdmin.java
+++ ModevonMorgenSP/src/Datenbankverwaltung/bearbeiteMaAdmin.java
@@ -1,5 +1,9 @@
 package Datenbankverwaltung;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class bearbeiteMaAdmin {
 	
 	/**
@@ -7,6 +11,69 @@ public class bearbeiteMaAdmin {
 	 * @author Julian
 	 */
 	
-	public static void bearbeiteMA
-
+	public static void bearbeiteMitarbeiterDaten(String Spalte, String Value, String PK) {
+		
+		
+		try {
+			
+			Connection con = Datenbankverwaltung.VerbindungDB.erstelleConnection();
+			PreparedStatement stmt = con.prepareStatement("update Mitarbeiter set ? = ? where Nutzernr = ? ");
+			
+			int pk = Integer.parseInt(PK);
+			
+			if ((Spalte == "Nutzernr")) {
+				int nummer = Integer.parseInt(Value);
+				
+				stmt.setString(1, Spalte);
+				stmt.setInt(2,nummer);
+				stmt.setInt(3, pk);
+				
+				stmt.execute();
+				
+				
+			}else if ((Spalte =="Adminnr")) {
+				int nummer = Integer.parseInt(Value);
+				
+				stmt.setString(1, Spalte);
+				stmt.setInt(2,nummer);
+				stmt.setInt(3, pk);
+				
+				stmt.execute();
+				
+			}else if ((Spalte == "Gehalt")) {
+				int gehalt = Integer.parseInt(Value);
+				
+				stmt.setString(1, Spalte);
+				stmt.setInt(2,gehalt);
+				stmt.setInt(3, pk);
+				
+				stmt.execute();
+				
+			}else if ((Spalte == "Plz")) {
+				int plz = Integer.parseInt(Value);
+				
+				stmt.setString(1, Spalte);
+				stmt.setInt(2,plz);
+				stmt.setInt(3, pk);
+				
+				stmt.execute();
+				
+			}else {
+				
+				stmt.setString(1, Spalte);
+				stmt.setString(2,Value);
+				stmt.setInt(3, pk);
+				
+				stmt.execute();
+				
+			}
+			
+			
+			
+		}catch (SQLException e) {
+			e.getMessage();
+		}
+		
+		
+	}
 }
