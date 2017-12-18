5cde6f1a4d5c5ce25ae4a26825a1abe4a55d6c68
agross1 <anna.gross98@gmail.com>
2017-12-18 22:31:03 +0100

    bearbeiet

--- ModevonMorgenSP/src/BestandskundenVerwaltung/BestandskundeStrg.java
+++ ModevonMorgenSP/src/BestandskundenVerwaltung/BestandskundeStrg.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
 public class BestandskundeStrg {
 	
 public static void aktualisiereName(String name, String nutzernr){
@@ -122,6 +123,67 @@ public static void aktualisiereName(String name, String nutzernr){
 		
 		Datenbankverwaltung.VerbindungDB.schlieﬂeVerbindung(con, stmt);
 		
+		}catch (SQLException e) {
+			e.getMessage();
+		}
+	}
+	
+	public static void neuerKunde(int nutzernr, String nachname, String vorname, String email, String straﬂe, String ort, int plz, String iban, int berechtigung, String passwort, int pss){
+		
+		Bestandskunde bk = new Bestandskunde( nutzernr, nachname,  vorname, email, straﬂe, ort,plz, iban, berechtigung, passwort,pss);
+		
+		
+		try {
+			
+			Connection con = Datenbankverwaltung.VerbindungDB.erstelleConnection();
+			
+			Statement stmt = con.createStatement();
+			
+			String sqlbefehl = "insert into Bestandskunde values ('"+nutzernr+"','"+nachname+"','"+vorname+"','"+email+"','"+straﬂe+"','"+ort+"','"+plz+"','"+iban+"','"+berechtigung+"','"+passwort+"','"+pss+"')";
+			
+			stmt.executeQuery(sqlbefehl);
+			
+			Datenbankverwaltung.VerbindungDB.schlieﬂeVerbindung(con, stmt);
+			
+			
+		}catch (SQLException e) {
+			e.getMessage();
+		}
+		
+	}
+
+	public static void lˆschenAccount(int nutzerNr) {
+		try {
+			
+			int kdnr = nutzerNr;
+			Connection con = Datenbankverwaltung.VerbindungDB.erstelleConnection();
+			
+			Statement stmt = con.createStatement();
+			
+			String sqlbefehel ="delete from Bestandskunde where nutzerNr = '"+kdnr+"'";
+			
+			stmt.executeQuery(sqlbefehel);
+			Datenbankverwaltung.VerbindungDB.schlieﬂeVerbindung(con, stmt);
+			
+			
+		}catch (SQLException e) {
+			e.getMessage();
+		}
+	}
+	
+	public static void anzeigenPunktestand(int nutzerNr){
+		int kdnr = nutzerNr;
+		try {
+
+			Connection con = Datenbankverwaltung.VerbindungDB.erstelleConnection();
+			
+			Statement stmt = con.createStatement();
+			
+			String sqlbefehel ="select PSS from Bestandskunde where nutzerNr = '"+kdnr+"'";
+			
+			stmt.executeQuery(sqlbefehel);
+			Datenbankverwaltung.VerbindungDB.schlieﬂeVerbindung(con, stmt);
+			
 		}catch (SQLException e) {
 			e.getMessage();
 		}
