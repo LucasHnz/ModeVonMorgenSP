f0b2f7c089fd0a10269332d9e130dc253aa54af4
agross1 <anna.gross98@gmail.com>
2017-12-18 22:58:55 +0100

    überarbeitet

--- ModevonMorgenSP/src/Bestellverwaltung/Bestellpositionssammlung.java
+++ ModevonMorgenSP/src/Bestellverwaltung/Bestellpositionssammlung.java
@@ -1,24 +1,67 @@
 package Bestellverwaltung;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 
 
-public class Bestellpositionssammlung {
-	protected Bestellposition bBestellposition;
+import Artikelverwaltung.Artikel;
 
-	HashMap<Integer,Bestellposition> Bestellpositionssammlung = new HashMap<Integer,Bestellposition>();
+
+
+public class Bestellpositionssammlung {
 	
-	public Bestellpositionssammlung(ResultSet rs) throws SQLException {
-		while (rs.next()) {
-			int aMenge = rs.getInt("Menge");
-			int posNr = rs.getInt("PosNr");
-			
+	static HashMap<Integer, Bestellposition> BestellpositionsSammlung = new HashMap<Integer, Bestellposition>();
+	
+	public static void füllenSammlung(ResultSet rs, String kateg) {
+		try{
+			while(rs.next()){
 		
-			Bestellpositionssammlung.put(bBestellposition.getPosNr(), bBestellposition);
+				int poNr = rs.getInt("PosNr");
+				int aMenge = rs.getInt("ArtikelMenge");
+				int bestellNr =rs.getInt("BestellNr");
+				double Preis = rs.getDouble("Preis");
+				Artikel aArtikel=rs.getObject("Artikel");
+				Bestellposition bpos = new Bestellposition (aArtikel, bestellNr, bestellNr);
+				
+				Bestellpositionssammlung.put(bpos.getPosNr(),bpos);
 			
+			}
+		}catch(SQLException e) {
+			System.out.println(e.getMessage());
 		}
-		
 	}
-}
+	
+	public static HashMap<Integer, Bestellposition> getBestellpositionssammlung(){
+		return Bestellpositionssammlung;
+	}
+	public static void removePosition(int posNr) {
+		Bestellpositionssammlung.remove(posNr);
+	}
+		
+	public static Artikel getArtikel(int posNr) {
+		return Bestellpositionssammlung.get(posNr);
+	}
+	/**
+	 * Fügt der Bestellpositionssammlung einen neuen Bestellposition hinzu.
+	 * @param kateg Die Kategorie des Artikels. Also Schuhe, Accessoires oder Kleidung
+	 * @param Positionsnr
+	 * @param Bestellnr
+	 * @param ArikelMenge
+	 * @param Preis
+	 * @return 
+	 */
+	public static void hinzufügenPosition(Artikel aArtikel,int aMenge, int posNr) {
+		
+		Bestellposition	bpos= new Bestellposition(aArtikel,aMenge, posNr);
+			Bestellpositionssammlung.put(posNr, bpos);
+	}
+	
+	
+			 
+		 
+	 }
+
+
