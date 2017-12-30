5be8869a0e7efd6b35bb2bea8f616f721d0348bb
Lucas <Lucas@10.136.194.227>
2017-12-30 19:03:07 +0100

    Hnz

---
+++ ModevonMorgenSP/src/Frontend/GUINeuerArtikel.java
@@ -0,0 +1,69 @@
+package Frontend;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.SystemColor;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+
+public class GUINeuerArtikel {
+	
+public void neuerArtikel(int artikelNummer) {
+		
+		JPanel panelArtikel = new JPanel();
+		panelArtikel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panelArtikel.setBackground(SystemColor.inactiveCaption);
+		panelArtikel.setBounds(66, 30, 680, 188);
+		GUIDamenSchuhe.panelDamenSchuhe.add(panelArtikel);
+		panelArtikel.setLayout(null);
+		
+		/*
+		ImageIcon icon = new ImageIcon(artikelBild);
+        int width = icon.getIconHeight() / 2;
+        int height = icon.getIconWidth() / 2;
+        Image img = icon.getImage().getScaledInstance(width, height, Image.SCALE_FAST);
+		*/
+		
+		JLabel labelArtikelBild = new JLabel("");
+		labelArtikelBild.setHorizontalAlignment(SwingConstants.CENTER);
+		labelArtikelBild.setVerticalAlignment(SwingConstants.TOP);
+		//labelArtikelBild.setIcon(new ImageIcon(img));
+		labelArtikelBild.setBounds(33, 25, 133, 135);
+		panelArtikel.add(labelArtikelBild);
+		
+		JLabel lblSchwarzeJackeDenim = new JLabel(artikelBezeichnung);
+		lblSchwarzeJackeDenim.setFont(new Font("Lucida Bright", Font.BOLD, 18));
+		lblSchwarzeJackeDenim.setBounds(203, 11, 213, 30);
+		panelArtikel.add(lblSchwarzeJackeDenim);
+		
+		JLabel lblNewLabel = new JLabel();
+		lblNewLabel.setText(String.valueOf(artikelPreis));
+		lblNewLabel.setFont(new Font("Lucida Bright", Font.BOLD, 15));
+		lblNewLabel.setBounds(203, 71, 101, 47);
+		panelArtikel.add(lblNewLabel);
+		
+		JButton btnZumArtikel = new JButton("Zum Artikel");
+		btnZumArtikel.setFont(new Font("Lucida Bright", Font.BOLD, 15));
+		btnZumArtikel.setBackground(Color.WHITE);
+		btnZumArtikel.setBounds(198, 130, 139, 30);
+		btnZumArtikel.addActionListener(this);
+		panelArtikel.add(btnZumArtikel);
+		
+		JLabel lblStatus = new JLabel(artikelVerfügbarkeit);
+		lblStatus.setForeground(new Color(0, 204, 51));
+		lblStatus.setFont(new Font("Lucida Bright", Font.BOLD, 14));
+		lblStatus.setBounds(203, 41, 147, 30);
+		panelArtikel.add(lblStatus);
+		
+		
+		
+	}
+	
+	
+	
+
+}
