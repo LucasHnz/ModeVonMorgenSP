a368b899081df2e76d863c25d14b21b657152975
agross1 <anna.gross98@gmail.com>
2018-01-04 00:47:23 +0100

    -

--- ModevonMorgenSP/src/Frontend/GUIGanzeBestellungAnzeigen.java
+++ ModevonMorgenSP/src/Frontend/GUIGanzeBestellungAnzeigen.java
@@ -3,5 +3,5 @@ package Frontend;
 public class GUIGanzeBestellungAnzeigen {
 //hier soll die ganze Bestellung mit den zukaufenden Artikeln angezeigt werden.
 	// mit einem Button bestellen soll dann die Methode Rechnung verschicken gestartet werden und
-	// die Bestellung in der Datenbank gespeichert werden.
+	// die Bestellung in der Datenbank gespeichert werden
 }
